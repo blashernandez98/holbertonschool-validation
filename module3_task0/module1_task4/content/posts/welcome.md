20c7caaf72bbd16719700ee1f7f76945728e9ce8
Blas Hernández <68179343+blashernandez98@users.noreply.github.com>
2023-01-09 17:24:33 -0300

    Delete module3_task0/module1_task4 directory

--- module3_task0/module1_task4/content/posts/welcome.md
+++
@@ -1,6 +0,0 @@
----
-title: "Welcome to Awesome Inc."
-date: 2023-01-08T17:29:57-03:00
-draft: true
----
-
--- module3_task0/module1_task4/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install -y wget
-apt-get install -y make
-wget https://github.com/gohugoio/hugo/releases/download/v0.85.0/hugo_0.85.0_Linux-64bit.deb
-dpkg -i hugo_0.85.0_Linux-64bit.deb
-make build
