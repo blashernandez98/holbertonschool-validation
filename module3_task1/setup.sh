13977ee50e4ddb17d02c39556fa6de2bb9a27a2b
blas <blashernandez98@gmail.com>
2023-01-11 17:47:25 -0300

    added sudo

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y wget
+sudo apt-get update
+sudo apt-get install -y wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.85.0/hugo_0.85.0_Linux-64bit.deb
-dpkg -i hugo_0.85.0_Linux-64bit.deb
+sudo dpkg -i hugo_0.85.0_Linux-64bit.deb
